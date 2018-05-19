f6e8a624cd132c686f741421989a94eb4abc1b87
Andrey Sokolov <keremet@solaris.kirov.ru>
2018-05-19 16:37:48 +0300

    Исправлена ссылка на названии сайта

--- furniture/resources/views/layouts/frontend.blade.php
+++ furniture/resources/views/layouts/frontend.blade.php
@@ -38,7 +38,7 @@
 		<nav class="gtco-nav" role="navigation">
 			<div class="row">
 				<div class="col-sm-2 col-xs-12">
-					<div id="gtco-logo"><a href="index.html">Mebelion <em>.</em></a></div>
+					<div id="gtco-logo"><a href="/">Mebelion <em>.</em></a></div>
 				</div>
 				<div class="col-xs-10 text-right menu-1 main-nav">
 					<ul class="nav navbar-nav">
